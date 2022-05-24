a7d8004dd2fba565b53efcac367f4205b29df565
mauricio <mauriciop173@hotmail.com>
2022-05-24 02:30:33 -0500

    Se agrega el proyecto

---
+++ codigo.js
@@ -0,0 +1,19 @@
+const user = [
+    {
+        name: 'Mauricio ',
+        profession: 'Engeneri'
+    },
+    {
+        name: 'Andres ',
+        profession: 'Chef'
+    },
+    {
+        name: 'Alejandra ',
+        profession: 'Medic'
+    },
+    {
+        name: 'Victor ',
+        profession: 'Mechanic'
+    },
+
+]
